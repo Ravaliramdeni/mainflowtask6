20a495413188d0a2c02b0fc0e493fac4c486c17f
Ravaliramdeni <142166163+Ravaliramdeni@users.noreply.github.com>
2024-07-10 10:02:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ImageGallery from './ImageGallery';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <ImageGallery />
+        </div>
+    );
+}
+
+export default App;
